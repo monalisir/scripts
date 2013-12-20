e278c9517e3e777c7fe11c02eaa1fa922aab8160
Kode.Cooper <monalisir@gmail.com>
2013-12-20 19:01:11 +0800

    add sdp 2 ccb script

--- ccb2sdp.py
+++ ccb2sdp.py
@@ -5,6 +5,8 @@ import json
 import plistlib
 from biplist import *
 from xml.etree import ElementTree
+from xml.etree.ElementTree import ElementTree as ETClass
+#from xml.etree.ElementTree import ElementTree,Element 
 
 '''
     How to use this:
@@ -118,7 +120,6 @@ def convertSDP2CCB(data, totalframes, rate, ccbfile):
             oProperty['value'] = frame['opacity']
             if ( not opacity['keyframes'] ) or opacity['keyframes'][-1]['value'] != frame['opacity'] :
                 opacity['keyframes'].append(oProperty)
-            #print item['tag'], frame['index'], oProperty
 
             #position property
             p = {}
@@ -311,13 +312,6 @@ def convertSDP2CCB(data, totalframes, rate, ccbfile):
         colorSetter['value'] = color['keyframes'][0]['value']
         spriteNode['properties'].append(colorSetter)
 
-        #tag
-        tagSetter = {}
-        tagSetter['name'] = 'tag'
-        tagSetter['type'] = 'Integer'
-        tagSetter['value'] = item['tag']
-        spriteNode['properties'].append(tagSetter)
-
         children.append(spriteNode)
 
     # read templates and insert children to templates
@@ -331,7 +325,6 @@ def convertSDP2CCB(data, totalframes, rate, ccbfile):
     if length > 10.0 :
         length = 10.0
     templatesRoot['sequences'][0]['length'] = length
-    templatesRoot['sequences'][0]['resolution'] = rate
 
     writePlist(templatesRoot, ccbfile)
     writePlist(templatesRoot, ccbfile.replace('.ccb', '.plist'))
@@ -346,29 +339,227 @@ def convertSDP2CCB(data, totalframes, rate, ccbfile):
         ccbfile.close()
     '''
 
+    '''
+    def getFrameInfo(frametype, frames) :
+        if frametype == DEF_ROTATION_TYPE :
+            for frame in frames :
+
+        if frametype == DEF_POSITION_TYPE :
+        if frametype == DEF_SCALE_TYPE :
+        if frametype == DEF_OPACITY_TYPE :
+        if frametype == DEF_COLOR_TYPE :
+        if frametype == DEF_SPRITFRAME_TYPE :
+        if frametype == DEF_SKEW_TYPE :
+    '''
+
 PLUTIL = '/usr/bin/plutil'
 TEXTUREPACKER = '/usr/local/bin/TexturePacker'
 CCBTEMPLATE = '/Users/cooper/Work/iOS/CocosBuilder-2.1-examples/CocosBuilderExample/CocosBuilderExample/Resources/Tests/sdp_template.ccb'
 
 filenames=os.listdir(os.getcwd())
 for filename in filenames:
-    if (filename.rfind(".sdp") < 0):
+    if (filename.rfind(".ccb") < 0):
         continue
 
-    # make temp ccb folder
-    ccbFolder = 'ccb'
-    if os.path.isdir(ccbFolder):
-        pass
-    else:
-        os.makedirs(ccbFolder)
+    # make temp sdp folder
+    sdpFolder = 'sdp'
+    if not os.path.isdir(sdpFolder):
+        os.makedirs(sdpFolder)
 
-    plstFile = ccbFolder + '/' + filename.replace(".sdp", ".plist")
-    ccbFile = ccbFolder + '/' + filename.replace(".sdp", ".ccb")
+    sdpFile = sdpFolder + '/' + filename.replace(".ccb", ".sdp")
 
     try:
-        print filename
-
         # get root element
+        ccbRoot = readPlist( filename )
+        sdpNodeList = []
+        for child in ccbRoot['nodeGraph']['children']:
+            if child['displayName'] == 'SDPNode':
+                sdpNodeList.append(child['children'])
+
+        if not sdpNodeList :
+            continue
+
+        sdpNode = sdpNodeList[0]
+
+        # create root node ---SkeletonParts
+        SkeletonParts = ElementTree.Element('SkeletonParts', attrib = {'version':'1.0'} )
+
+        # create SkeletonTree
+        SkeletonTree = ElementTree.SubElement(SkeletonParts, 'SkeletonTree', attrib = {'version':'1.0'})
+
+        # create SkeletonAction
+        SkeletonAction = ElementTree.SubElement(SkeletonParts, 'SkeletonAction', attrib = {'version':'1.0', 'TotalFrame':"16", 'Rate' : "13"})
+
+        # create SkeletonSkin
+        SkeletonSkin = ElementTree.SubElement(SkeletonParts, 'SkeletonSkin', attrib = {'version':'1.0'})
+
+        for item in sdpNode :
+            for propInfo in item['properties'] :
+                propertyName = propInfo['name']
+                propertyValue = propInfo['value']
+
+                if propertyName == 'tag' :
+                    p_tag = int(propertyValue)
+                if propertyName == 'displayFrame' :
+                    p_sprite = propertyValue[1]
+                if propertyName == 'anchorPoint' :
+                    p_anchor = propertyValue
+                if propertyName == 'position' :
+                    p_position = propertyValue
+                if propertyName == 'scale' :
+                    p_scale = propertyValue
+                if propertyName == 'rotation' :
+                    p_rotation = propertyValue
+                if propertyName == 'skew' :
+                    p_skew = propertyValue
+                if propertyName == 'opacity' :
+                    p_opacity = propertyValue
+                if propertyName == 'color' :
+                    p_color = propertyValue
+
+
+            NodeName = ElementTree.SubElement( SkeletonTree, 'NodeName', attrib = {'Tag':str(p_tag), 'Name':item['displayName']} )
+
+            NodeData = ElementTree.SubElement( SkeletonAction, 'NodeData', attrib = {'Tag':str(p_tag), 'AnchorX':str(p_anchor[0]), 'AnchorY':str(p_anchor[1])} )
+
+            KeyFrameAnim = ElementTree.SubElement( SkeletonSkin, 'KeyFrameAnim', attrib = { 'Tag':str(p_tag) } )
+
+            animationFrames = item['animatedProperties']['0']
+
+            frameInfo = {}
+            if animationFrames.has_key('rotation') :
+                for r in animationFrames['rotation']['keyframes'] :
+                    idx = str(r['time'])
+                    if not frameInfo.has_key(idx) :
+                        frameInfo[idx] = {}
+                    frameInfo[idx]['rotation'] = r['value']
+            if animationFrames.has_key('position') :
+                for p in animationFrames['position']['keyframes'] :
+                    idx = str(p['time'])
+                    if not frameInfo.has_key(idx) :
+                        frameInfo[idx] = {}
+                    frameInfo[idx]['position'] = p['value']
+            if animationFrames.has_key('scale') :
+                for s in animationFrames['scale']['keyframes'] :
+                    idx = str(s['time'])
+                    if not frameInfo.has_key(idx) :
+                        frameInfo[idx] = {}
+                    frameInfo[idx]['scale'] = s['value']
+            if animationFrames.has_key('opacity') :
+                for o in animationFrames['opacity']['keyframes'] :
+                    idx = str(o['time'])
+                    if not frameInfo.has_key(idx) :
+                        frameInfo[idx] = {}
+                    frameInfo[idx]['opacity'] = o['value']
+            if animationFrames.has_key('color') :
+                for c in animationFrames['color']['keyframes'] :
+                    idx = str(c['time'])
+                    if not frameInfo.has_key(idx) :
+                        frameInfo[idx] = {}
+                    frameInfo[idx]['color'] = c['value']
+            if animationFrames.has_key('displayFrame') :
+                for f in animationFrames['displayFrame']['keyframes'] :
+                    idx = str(f['time'])
+                    if not frameInfo.has_key(idx) :
+                        frameInfo[idx] = {}
+                    frameInfo[idx]['displayFrame'] = f['value'][1]
+            if animationFrames.has_key('skew') :
+                for x in animationFrames['skew']['keyframes'] :
+                    idx = str(x['time'])
+                    if not frameInfo.has_key(idx) :
+                        frameInfo[idx] = {}
+                    frameInfo[idx]['skew'] = x['value']
+
+            # convert frame dict to list
+            frameList = []
+            for frameIdx in sorted(frameInfo.keys()) :
+                frameInfo[frameIdx]['Frame'] = frameIdx
+
+                if not 'rotation' in frameInfo[frameIdx] :
+                    if not frameList :
+                        frameInfo[frameIdx]['rotation'] = p_rotation
+                    else :
+                        frameInfo[frameIdx]['rotation'] = frameList[-1]['rotation']
+
+                if not 'position' in frameInfo[frameIdx] :
+                    if not frameList :
+                        frameInfo[frameIdx]['position'] = p_position
+                    else :
+                        frameInfo[frameIdx]['position'] = frameList[-1]['position']
+                    
+                if not 'scale' in frameInfo[frameIdx] :
+                    if not frameList :
+                        frameInfo[frameIdx]['scale'] = p_scale
+                    else :
+                        frameInfo[frameIdx]['scale'] = frameList[-1]['scale']
+
+                if not 'opacity' in frameInfo[frameIdx] :
+                    if not frameList :
+                        frameInfo[frameIdx]['opacity'] = p_opacity
+                    else :
+                        frameInfo[frameIdx]['opacity'] = frameList[-1]['opacity']
+
+                if not 'color' in frameInfo[frameIdx] :
+                    if not frameList :
+                        frameInfo[frameIdx]['color'] = p_color
+                    else :
+                        frameInfo[frameIdx]['color'] = frameList[-1]['color']
+
+                if not 'skew' in frameInfo[frameIdx] :
+                    if not frameList :
+                        frameInfo[frameIdx]['skew'] = p_skew
+                    else :
+                        frameInfo[frameIdx]['skew'] = frameList[-1]['skew']
+
+                if not 'displayFrame' in frameInfo[frameIdx] :
+                    if not frameList :
+                        frameInfo[frameIdx]['displayFrame'] = p_sprite
+                    elif ( frameList[-1].has_key('displayFrame') ) and ( 'displayFrame' in frameInfo[frameIdx] ):
+                        if (frameInfo[frameIdx]['displayFrame'] != frameList[-1]['displayFrame']) :
+                            frameInfo[frameIdx]['displayFrame'] = frameList[-1]['displayFrame']
+                    elif 'displayFrame' in frameInfo[frameIdx]:
+                        del frameInfo[frameIdx]['displayFrame']
+                                                                                                
+                frameList.append(frameInfo[frameIdx])
+            
+                data = frameList[-1]
+                i = int(float(data['Frame']) * 13)
+                print i
+                FrameData = ElementTree.SubElement( NodeData, 'FrameData', attrib = {'Frame'  : str(i), 
+                                                                                     'PosX'   : str(list(data['position'])[0]),
+                                                                                     'PosY'   : str(list(data['position'])[1]),
+                                                                                     'ScaleX' : str(list(data['scale'])[0]),
+                                                                                     'ScaleY' : str(list(data['scale'])[1]),
+                                                                                     'SkewX'  : str(list(data['skew'])[0]),
+                                                                                     'SkewY'  : str(list(data['skew'])[1]),
+                                                                                     'Rot'    : str(data['rotation']),
+                                                                                     'Color'  : str(data['color']),
+                                                                                     'Opacity': str(data['opacity']),
+                                                                                     'Visible': '0' } )
+
+    
+                if 'displayFrame' in data :
+                    FrameData = ElementTree.SubElement( KeyFrameAnim, 'FrameData', attrib = {'Frame'  : str(i), 'Name' :data['displayFrame'] } )
+                    print data
+
+
+
+        #ElementTree.dump(SkeletonParts)
+        xmlRoot = ETClass(SkeletonParts)
+
+        #tree = ElementTree(SkeletonParts)
+        xmlRoot.write(sdpFile, encoding="utf-8", xml_declaration=True)
+
+        continue
+        '''
+        DEF_ROTATION_TYPE = 2
+        DEF_POSITION_TYPE = 3
+        DEF_SCALE_TYPE = 4
+        DEF_OPACITY_TYPE = 5
+        DEF_COLOR_TYPE = 6
+        DEF_SPRITFRAME_TYPE = 7
+        DEF_SKEW_TYPE = 8
+        '''
         root = ElementTree.parse(filename).getroot()
         spriteList = []
 
